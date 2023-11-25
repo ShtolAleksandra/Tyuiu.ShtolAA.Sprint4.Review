63aa817e7e999ef66d793a8726355e201dad9d1e
aleksandrastol8@gmail.com <aleksandrastol8@gmail.com>
2023-11-25 11:59:14 +0500

    Add project files.

---
+++ Tyuiu.ShtolAA.Sprint4.TaskReview.V12.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShtolAA.Sprint4.TaskReview.V12.Lib;
+
+namespace Tyuiu.ShtolAA.Sprint4.TaskReview.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            string str = "658932125478";
+            int rows = 3;
+            int columns = 4;
+            int wait = 30;
+            int res = ds.Calculate(rows, columns, str);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShtolAA.Sprint4.TaskReview.V12/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShtolAA.Sprint4.TaskReview.V12.Lib;
+
+namespace Tyuiu.ShtolAA.Sprint4.TaskReview.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Штоль А.А. | ИИПб-23-3 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: TaskReview                                                        *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнила: Штоль Александра Алексеевна | ИИПб-23-3                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '658932125478'. Преобразуйте ее в     *");
+            Console.WriteLine("* матрицу 3 на 4 и подсчитайте сумму нечетных чисел.                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string str = "658932125478";
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+            int index = 0;
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма нечетных чисел массива = " + ds.Calculate(rows, columns, str));
+
+            Console.ReadLine();
+        }
+    }
+}
